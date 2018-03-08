9614288819f585b68fb32f6cc9d905a9f17e46cc
pparksuuu <supr2000@gmail.com>
2018-03-08 11:44:23 +0900

    '18.03.08'

--- bitcamp-java-basic/src/step04/Exam01_4.java
+++ bitcamp-java-basic/src/step04/Exam01_4.java
@@ -5,9 +5,18 @@ public class Exam01_4 {
     public static void main(String[] args) {
         int i = 5;
         int j = 2;
-        float r = i / j;
+        float r = i / j; //int와 int의 연산 결과는 항상 int.
+                         //따라서 r 변수에 넣기 전에 이미 결과는 정수 2.
+                         //그래서 출력할 때 2.0이 된다.
         System.out.println(r);
-        System.out.println((int)r);
-         
+
+        //해결책 ! 
+        /**
+         -변수에 들어 있는 값을 다른 타입으로 바꿔라.
+         "형변환(type conversion = type casting)"
+         */
+        r = (float)i / (float)j;
+        System.out.println(r);
+        
         }
     }
