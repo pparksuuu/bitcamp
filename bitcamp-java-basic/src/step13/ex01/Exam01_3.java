41d54536d6445e6cf9c9913ede67e3206b628a2f
pparksuuu <supr2000@gmail.com>
2018-03-30 16:05:17 +0900

    '18.03.30'

--- bitcamp-java-basic/src/step13/ex01/Exam01_3.java
+++ bitcamp-java-basic/src/step13/ex01/Exam01_3.java
@@ -1,19 +1,32 @@
 // 제네릭(Generic) - 문법 정리
 package step13.ex01;
 
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 
 public class Exam01_3 {
     public static void main(String[] args) {
+        HashMap map = new HashMap();
+        map.put("aaa", "문자열");
+        map.put(new Integer(100), new Member("홍길동", 20));
         
-        ArrayList<Member> list = new ArrayList<Member>();
-        list.add(new Member("홍길동", 20));
+        // 명확하게 Object 타입을 지정하는 것이 좋다.
+        HashMap<Object, Object> map2 = new HashMap<>();
+        map2.put("aaa", "문자열");
+        map2.put(new Integer(100), new Member("홍길동", 20));
+
+        
+        // key : String
+        // value : Member
+        HashMap<String, Member> map3 = new HashMap<>();
+        //map3.put("aaa", "문자열"); //컴파일 오류!
+        //map3.put(new Integer(100), new Member("홍길동", 20));// 컴파일 오류
+        map3.put("aaa", new Member("홍길동", 20)); //OK!
+        
+        Member m = map3.get("aaa");
         
         
-        ArrayList<Member> list2 = new ArrayList</*Member*/>();//OK!
+
         
-        ArrayList<Member> list3;
-        list3 = new ArrayList<>(); //OK!
+        
     }
 }
---
+++ bitcamp-java-basic/src/step13/ex01/Exam01_4.java
@@ -0,0 +1,19 @@
+// 제네릭(Generic) - 문법 정리
+package step13.ex01;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Exam01_4 {
+    public static void main(String[] args) {
+        
+        ArrayList<Member> list = new ArrayList<Member>();
+        list.add(new Member("홍길동", 20));
+        
+        
+        ArrayList<Member> list2 = new ArrayList</*Member*/>();//OK!
+        
+        ArrayList<Member> list3;
+        list3 = new ArrayList<>(); //OK!
+    }
+}
