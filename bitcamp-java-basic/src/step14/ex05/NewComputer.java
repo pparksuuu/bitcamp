f866acc9ffd438658fb82db8de9a55d132dc225c
pparksuuu <supr2000@gmail.com>
2018-04-02 16:42:39 +0900

    '18.04.02'

---
+++ bitcamp-java-basic/src/step14/ex05/NewComputer.java
@@ -0,0 +1,11 @@
+package step14.ex05;
+
+public class NewComputer implements Computer {
+    public void computer() {
+        System.out.println("새 컴퓨터...");
+    }
+    
+    public void touch() {
+        System.out.println("오호라... 터치가 되네..이거 서피스 프로인가?");
+    }
+}
