53c9d9f253bed332cc51d61369d5c8e466c4908d
pparksuuu <supr2000@gmail.com>
2018-04-03 09:49:05 +0900

    '18.04.03'

---
+++ bitcamp-java-basic/src/step14/ex06/Exam03.java
@@ -0,0 +1,9 @@
+package step14.ex06;
+
+public class Exam03 extends Exam01 {
+    public static void main(String[] args) {
+        A r1 = new Exam01(); //OK!
+        A r2 = new Exam02();
+        
+    }
+}
