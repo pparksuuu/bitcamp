94cc5c80deabaeb4cf2deb898e3960581f83dcdd
pparksuuu <supr2000@gmail.com>
2018-04-17 10:55:51 +0900

    '18.04.17'

--- bitcamp-java-basic/src/step23/ex05/StatelessClient2.java
+++ bitcamp-java-basic/src/step23/ex05/StatelessClient2.java
@@ -1,4 +1,4 @@
-// Stateless와 Stateful 의 비교 
+// Stateless 클라이언트 - 클라이언트 식별 번호 적용
 package step23.ex05;
 
 import java.io.PrintStream;
@@ -8,23 +8,30 @@ import java.util.Scanner;
 // stateless 방식의 특징
 // => 요청 할 때마다 서버와 연결한다. 응답 받은 후 서버와의 연결을 끊는다. 
 // 
-public class StatelessClient {
+public class StatelessClient2 {
     public static void main(String[] args) throws Exception {
+        int clientId = 0; // 서버로부터 클라이언트 ID를 발급받기 전에는 0으로 설정한다.
         // 합계 계산을 요청하는 클라이언트를 만들어보자!
         try (Scanner keyScan = new Scanner(System.in)) {
             while (true) {
                 System.out.print("값? ");
-                String str = keyScan.nextLine(); // 키보드에서 값을 입력 받아
+                String value = keyScan.nextLine(); // 키보드에서 값을 입력 받아
                 try (
                     Socket socket = new Socket("localhost", 8888);
                     PrintStream out = new PrintStream(socket.getOutputStream());
                     Scanner in = new Scanner(socket.getInputStream())) {
     
-                    out.println(str); // 서버에 보낸다. 
-                    if (str.equals("")) { // 만약 값을 입력하지 않았으면 
+                    out.println(value); // 서버에 값을 보낸다. 
+                    out.printf("%d\n", clientId);
+                    
+                    if (value.equals("")) { // 만약 값을 입력하지 않았으면 
                         System.out.println(in.nextLine()); // 서버가 보낸 결과를 출력한다.
                         break;
                     }
+                    
+                    //서버가 보낸 값은 항상 클라이언트 식별번호(ID)이기 때문에
+                    // 받은 값을 보관해야 한다.
+                    clientId = Integer.parseInt(in.nextLine());
                 }
             }
         }
---
+++ bitcamp-java-basic/src/step23/ex06/ConnectionOrientedServer.java
@@ -0,0 +1,34 @@
+// connection-oriented 서버 - 연결 후 데이터 송수신
+package step23.ex06;
+
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+//연결지향(conn
+public class ConnectionOrientedServer {
+    public static void main(String[] args) throws Exception {
+        System.out.println("서버 실행 중...");
+        ServerSocket ss = new ServerSocket(8888);
+        
+        Socket socket = ss.accept();
+        System.out.println("클라이언트가 연결되었음!");
+        
+        Scanner in = new Scanner(socket.getInputStream());
+        PrintStream out = new PrintStream(socket.getOutputStream());
+        
+        String str = in.nextLine();
+        System.out.println("데이터 수신 완료!");
+        
+        out.println(str);
+        System.out.println("데이터 송신 완료!");
+        
+        in.close();
+        out.close();
+        socket.close();
+        System.out.println("클라이언트 연결 끊기!");
+        
+        ss.close();
+    }
+}
