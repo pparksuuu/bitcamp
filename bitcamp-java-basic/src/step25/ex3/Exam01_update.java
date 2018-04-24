b8be433135616a857a42cd823939f4ab52e287ec
pparksuuu <supr2000@gmail.com>
2018-04-24 11:02:26 +0900

    '18.04.24'

---
+++ bitcamp-java-basic/src/step25/ex3/Exam01_update.java
@@ -0,0 +1,31 @@
+// JDBC 프로그래밍 개요 - DBMS에 SQL문 보내기 : insert
+package step25.ex3;
+
+import java.util.Scanner;
+
+public class Exam01_update {
+    public static void main(String[] args) throws Exception {
+        Scanner keyScan = new Scanner(System.in);
+        
+        Board board = new Board();
+        
+        System.out.print("변경할 게시물 번호? ");
+        board.setNo(Integer.parseInt(keyScan.nextLine()));
+        
+        System.out.print("제목? ");
+        board.setTitle(keyScan.nextLine());
+        
+        System.out.print("내용? ");
+        board.setContent(keyScan.nextLine());
+        
+        try {
+            BoardDao boardDao = new BoardDao();
+            int count = boardDao.update(board);
+            System.out.printf("%d개 변경 성공!", count);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        keyScan.close();
+    }
+}
