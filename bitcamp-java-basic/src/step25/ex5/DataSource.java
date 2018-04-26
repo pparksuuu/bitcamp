cc4099aec8f756293af78cbe82bbcf3c2fdda77b
pparksuuu <supr2000@gmail.com>
2018-04-26 14:19:40 +0900

    '18.04.26'

--- bitcamp-java-basic/src/step25/ex5/DataSource.java
+++
@@ -1,9 +0,0 @@
-
-package step25.ex5;
-
-import java.sql.Connection;
-
-public interface DataSource {
-    Connection getConnection() throws Exception;
-    void returnConnection(Connection con);
-}
