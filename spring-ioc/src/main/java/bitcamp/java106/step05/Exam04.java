85d2996fc997d5b0694f394f421122cb7fbecd41
pparksuuu <supr2000@gmail.com>
2018-05-04 15:51:42 +0900

    '18.05.04'

---
+++ spring-ioc/src/main/java/bitcamp/java106/step05/Exam04.java
@@ -0,0 +1,18 @@
+// 객체 생성
+package bitcamp.java106.step05;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import bitcamp.java106.BeanUtils;
+
+public class Exam04 {
+    public static void main(String[] args) {
+        ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
+                "bitcamp/java106/step05/application-context-04.xml");
+        
+        BeanUtils.printBeanNames(iocContainer);
+        
+        System.out.println(iocContainer.getBean("c1"));
+    }
+}
