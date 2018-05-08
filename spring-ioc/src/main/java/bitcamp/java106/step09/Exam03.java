72ba556733f8e7a79bcb50f02f9f14fdee5af88d
pparksuuu <supr2000@gmail.com>
2018-05-08 16:19:31 +0900

    '18.05.08'

---
+++ spring-ioc/src/main/java/bitcamp/java106/step09/Exam03.java
@@ -0,0 +1,18 @@
+// 객체 자동 생성 - 특정 패키지의 클래스에 대해 객체 생성하지 않기
+package bitcamp.java106.step09;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import bitcamp.java106.BeanUtils;
+
+public class Exam03 {
+    public static void main(String[] args) {
+        // <context:annotation-config> 태그 생략하기
+        ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
+                "bitcamp/java106/step09/application-context-03.xml");
+
+        BeanUtils.printBeanNames(iocContainer);
+        
+    }
+}
