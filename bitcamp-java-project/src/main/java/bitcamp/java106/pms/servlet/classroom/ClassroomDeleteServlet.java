62efff91d720379e59e8a27149fe96198cdc0715
pparksuuu <supr2000@gmail.com>
2018-05-21 09:55:28 +0900

    '18.05.21'

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/classroom/ClassroomDeleteServlet.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/classroom/ClassroomDeleteServlet.java
@@ -1,8 +1,6 @@
-// Controller 규칙에 따라 메서드 작성
 package bitcamp.java106.pms.servlet.classroom;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -16,7 +14,8 @@ import bitcamp.java106.pms.servlet.InitServlet;
 
 @SuppressWarnings("serial")
 @WebServlet("/classroom/delete")
-public class ClassroomDeleteController extends HttpServlet {
+public class ClassroomDeleteServlet extends HttpServlet {
+    
     ClassroomDao classroomDao;
     
     @Override
@@ -27,33 +26,29 @@ public class ClassroomDeleteController extends HttpServlet {
     @Override
     protected void doGet(
             HttpServletRequest request, 
-            HttpServletResponse response)
-            throws ServletException, IOException {
-        
-            response.setContentType("text/html;charset=UTF-8");
-        
+            HttpServletResponse response) throws ServletException, IOException {
+
         try {
             int no = Integer.parseInt(request.getParameter("no"));
             int count = classroomDao.delete(no);
-            
             if (count == 0) {
-                throw new Exception("유효하지 않은 게시물 번호입니다.");
+                throw new Exception("<p>해당 강의가 없습니다.</p>");
             }
             response.sendRedirect("list");
             
         } catch (Exception e) {
-            
             RequestDispatcher 요청배달자 = request.getRequestDispatcher("/error");
-            
             request.setAttribute("error", e);
-            request.setAttribute("title", "수업 삭제 실패");
-            // 다른 서블릿으로 실행을 위임할 때,
-            // 이전까지 버퍼로 출력한 데이터는 버린다.
+            request.setAttribute("title", "강의 삭제 실패!");
             요청배달자.forward(request, response);
         }
     }
+    
 }
 
+//ver 39 - forward 적용
+//ver 38 - redirect 적용
+//ver 37 - 컨트롤러를 서블릿으로 변경
 //ver 31 - JDBC API가 적용된 DAO 사용
 //ver 28 - 네트워크 버전으로 변경
 //ver 26 - ClassroomController에서 delete() 메서드를 추출하여 클래스로 정의.
