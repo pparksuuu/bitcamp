d2fa2a726d79438ac6ffae8e7bfb1f8d3f538ce2
pparksuuu <supr2000@gmail.com>
2018-06-04 15:53:31 +0900

    '18.06.04'

---
+++ bitcamp-spring-webmvc/src/main/java/bitcamp/mvc/web/Exam01_3.java
@@ -0,0 +1,27 @@
+// @RequestMapping - 한 클래스에 여러 개의 요청 핸들러 두기
+package bitcamp.mvc.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller 
+@RequestMapping("/exam01_3")
+public class Exam01_3 {
+
+    // 한 클래스에 여러 개의 요청 핸들러를 정의할 때는
+    // 각각의 RequestMapping 애노테이션에 URL을 지정한다.
+    @RequestMapping("/m1")
+    @ResponseBody  
+    public String m1() {
+
+        return "Exam01_3.m1() 실행";
+    }
+
+    @RequestMapping("/m2")
+    @ResponseBody 
+    public String m2() {
+
+        return "Exam01_3.m2() 실행";
+    }
+}
