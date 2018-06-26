e595fcf116bfbfc52675f9fbf666a36c666897f1
pparksuuu <supr2000@gmail.com>
2018-06-26 17:31:27 +0900

    '18.06.26'

--- bitcamp-java-project/src/main/webapp/html2/header.js
+++ bitcamp-java-project/src/main/webapp/html2/header.js
@@ -9,7 +9,14 @@ function loadLoginUser() {
 	$.getJSON("/bitcamp-java-project/json/auth/loginUser", (data) => {
 		if (data == "")
 			location.href = "/bitcamp-java-project/html2/auth/login.html";
-		else 
-			$("#username").text = data.id;
+		else {
+			$("#username").text(data.id);
+			$("#logoutBtn").click((e) => {
+				e.preventDefault(); // 클릭했을 때 원래 하던 일이 있는데 그것을 하지 말라!
+				$.get("/bitcamp-java-project/json/auth/logout", () => {
+					location.href = "/bitcamp-java-project/html2/auth/login.html";
+				});
+			});
+		}
 	});
 }
