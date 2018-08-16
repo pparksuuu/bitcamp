616e11f92de013ba8df1f12dad0e167e13cf44b5
pparksuuu <supr2000@gmail.com>
2018-08-16 15:58:45 +0900

    .

--- bitcamp-java-project/src/main/webapp/html7/js/common.js
+++ bitcamp-java-project/src/main/webapp/html7/js/common.js
@@ -1,1 +1,2 @@
-var serverRoot = "http://localhost:8888/bitcamp-java-project";
+//var serverRoot = "http://localhost:8888/bitcamp-java-project";
+var serverRoot = "http://www.suhyeon.xyz:8888/bitcamp-java-project";
